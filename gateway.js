5bdde36eaa9b009a7cec5b1a4c45c93fd33b322c
nikitapermyakov <n.permikov@uzum.com>
2023-05-13 16:10:54 +0300

    Add logging request

--- gateway.js
+++ gateway.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
+
 const app = express()
 const port = 80
 
@@ -8,19 +9,30 @@ const botName = 'bot6222494632';
 const botToken = 'AAG1I5npTLlqo3cl_4jgredo_ptBjHd2ajw';
 const chatId = '-1001883224861';
 
-const sendMsgTg = (text) => {
+const sendRequest = (url, labelLog) => {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.telegram.org/${botName}:${botToken}/sendMessage?chat_id=${chatId}&text=${text}`);
+    xhr.open('GET', url);
+    xhr.onload = function () {
+        console.log(labelLog + ': ' + xhr.status);
+    };
     xhr.send();
+}
+
+const sendMsgTg = (text) => {
+    sendRequest(
+        `https://api.telegram.org/${botName}:${botToken}/sendMessage?chat_id=${chatId}&text=${text}`,
+        'sendMsgTg'
+    );
 };
 
 const sendMsgPhoneTg = (phone, text) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.telegram.org/${botName}:${botToken}/sendMessage?chat_id=${chatId}&text=${phone} - ${text}`);
-    xhr.send();
+    sendRequest(
+        `https://api.telegram.org/${botName}:${botToken}/sendMessage?chat_id=${chatId}&text=${phone} - ${text}`,
+        'sendMsgPhoneTg'
+    );
 };
 
-const checkQueryParam = (param) => (param === undefined || param === '') ? false : true;
+const checkQueryParam = (param) => !(param === undefined || param === '');
 
 app.get('/', (req, res) => {
     const text = req.query.text;
@@ -40,4 +52,6 @@ app.get('/', (req, res) => {
     res.send(responseText);
 });
 
-app.listen(port);
+app.listen(port, () => {
+    console.log('Listening on port: ' + port);
+});
